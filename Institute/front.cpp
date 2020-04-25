e23c9ac07fc0cbfb3dab8f48ec2bc4e5c456656c
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2020-04-25 14:05:24 +0530

    void frend()
    frend() is added for front looks

---
+++ Institute/front.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+enum st {op1 , op2 , op3 , op4};
+void frend() {
+	string op1 = { " 1) Create a Record " }, op2 = { " 2) Modify a Record " }, op3 = { " 3) Add a Record " };
+	string op;
+}
