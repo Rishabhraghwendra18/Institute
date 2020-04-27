d88f4f1cabdd168915e451554b8bba1d59584661
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2020-04-27 21:27:53 +0530

    Middle_top_finished

--- Institute/front.cpp
+++ Institute/front.cpp
@@ -3,7 +3,26 @@
 using namespace std;
 
 void frend() {
-	for (int i = 1; i < 11; i++)
+	int c = 0;
+	for (int m = 1; m <=5; m++)
+	{
+		cout << setw(59);
+		for (int r = 1; r <= 25; r++)
+		{
+			if (m == 3 &&c<1)
+			{
+				c++;
+				cout << "\n";
+				cout << setw(80) << "RISHIKUL INSTITUTE\n";
+				break;
+			}
+			else
+				cout << "$";
+		}
+		cout << "\n";
+	}
+
+	/**for (int i = 1; i < 11; i++)
 	{
 		for (int j = 1; j < 11; j++)
 		{
@@ -21,5 +40,5 @@ void frend() {
 		}
 		cout << '\n';
 
-	}
+	}**/
 }
