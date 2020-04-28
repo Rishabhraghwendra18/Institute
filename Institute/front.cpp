09281cd09279b4ca06abfa82408ad325834754c7
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2020-04-28 21:13:10 +0530

    Front_End_completed
    front end development completed successfully

--- Institute/front.cpp
+++ Institute/front.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 #include<iomanip>
+
 using namespace std;
 
 void frend() {
+	string op1 = "Create fresh record", op2 = "Delete a Record", op3 = "Update a Record", op4 = "Append Record", op5 = "Search a Record";
 	int c = 0;
 	for (int m = 1; m <=5; m++)
 	{
@@ -21,24 +23,81 @@ void frend() {
 		}
 		cout << "\n";
 	}
+	cout << "Choice respective option number to perform task : \n\n";
 
-	/**for (int i = 1; i < 11; i++)
+	for (int i = 1; i < 10; i++)
 	{
-		for (int j = 1; j < 11; j++)
+		for (int j = 1; j < 16; j++)
 		{
-			if (i >= 3 && i <= 6)
+			if (i >= 3 && i <= 7)
 			{
-				cout << setw(4)<< "Hi";
-				for (int k = 0; k < 2; k++)
+				//cout << setw(4);
+				if (i == 3)
 				{
-					cout << setw(4) << " *";
+					cout <<" 1) "<< op1; 
+					
+					cout << setw(4);
+					for (int k = 0; k < 2; k++)
+					{
+						//cout << setw(3) << "*";
+						cout<< "* ";
+					}
+					cout << '\n'; i++;
 				}
-				cout << '\n'; i++;
+				else if (i == 4)
+				{
+					cout << " 2) " << op3; 
+					
+					cout << setw(8);
+					for (int k = 0; k < 2; k++)
+					{
+						//cout << setw(3) << "*";
+						cout << "* ";
+					}
+					cout << '\n'; i++;
+				}
+				else if (i == 5)
+				{
+					cout << " 3) " << op2; 
+					
+					cout << setw(8);
+					for (int k = 0; k < 2; k++)
+					{
+						//cout << setw(3) << "*";
+						cout << "* ";
+					}
+					cout << '\n'; i++;
+				}
+				else if (i == 6)
+				{
+					cout << " 4) " << op4; 
+					
+					cout << setw(10);
+					for (int k = 0; k < 2; k++)
+					{
+						//cout << setw(3) << "*";
+						cout << "* ";
+					}
+					cout << '\n'; i++;
+				}
+				else if (i == 7)
+				{
+					cout << " 5) " << op5; 
+					
+					cout << setw(8);
+					for (int k = 0; k < 2; k++)
+					{
+						//cout << setw(3) << "*";
+						cout << "* ";
+					}
+					j = 0;
+					cout << '\n'; i++;
+				}
 			}
-			else
-				cout << " *";
+				else
+					cout << " *";
 		}
 		cout << '\n';
 
-	}**/
+	}
 }
