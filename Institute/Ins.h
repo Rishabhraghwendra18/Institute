8f055f7e432cfd97b8f10a15f93cf55319b6bc70
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2020-04-30 20:45:09 +0530

    getting error

--- Institute/Ins.h
+++ Institute/Ins.h
@@ -5,7 +5,7 @@ using namespace std;
 class Ins
 {
 	int admin,fees;
-	string name,join_date,batch;
+	string name,join_date,batch,Class;
 	char gender;
 public:
 	Ins() {
@@ -16,14 +16,14 @@ public:
 		ofstream fout;
 		fout.open("Institute_data", ios::out || ios::binary);
 		fout.write((char*)& temp, sizeof(temp));
-		cout << "Inputted data successfully\n";
+		cout << "Inputed data successfully\n";
+		fout.close();
 	}
 	void input() {
 		cout << "Enter name : ";
 		getline(cin, name);
 		cout << "Enter admission number : ";
 		cin >> admin;
-
 		cout << "Enter student gender(Press 'M' for male and 'F' for female) : ";
 		cin >> gender;
 		if (gender == 'M' || gender == 'm')
@@ -34,6 +34,8 @@ public:
 			getline(cin, batch);
 			cout << "Enter his fees : ";
 			cin >> fees;
+			cout << "Enter his class : ";
+			getline(cin, Class);
 		}
 		else
 		{
@@ -43,15 +45,57 @@ public:
 			getline(cin, batch);
 			cout << "Enter her fees : ";
 			cin >> fees;
+			cout << "Enter her class : ";
+			getline(cin, Class);
 		}
-
+		
+	}
+	void diplay() {
+		cout << "Name of student : " << name;
+		cout << "\nAdmission Number : " << admin;
+		cout << "\nEnter his class : " << Class;
+		cout << "\nGender : " << gender;
+		cout << "\nJoining date : " << join_date;
+		cout << "\nBatch : " << batch;
+	}
+	void disp_all() {
+		ifstream ifs("Institute_data", ios::in || ios::binary);
+		Ins at;
+		while (ifs.read((char*)& at, sizeof(at)))
+		{
+			at.diplay();
+		}
+	}
+	int ad() {
+		return admin;
 	}
 	
-};
+}s1;
 void create()
 {
 	ofstream ofs("Institute_data", ios::binary);
 	cout << "\nFile created successfully\n";
 	ofs.close();
+}
+void search( int adm_n)
+{
+	int n= 0;
+	ifstream s;
+	s.open("Institute_data", ios::in | ios::binary);
+	while(s.read((char*)&s1,sizeof(s1)))
+	{
+		if (s1.ad() == adm_n)
+		{
+			s1.diplay();
+			n = 0;
+			break;
+			
+		}
+		else
+			n++;
+	}
+	if (n != 0) {
+		cout << "Not found";
+	}
 }
 
